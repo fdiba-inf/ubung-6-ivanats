978ebae3e1e7688a4af9945dfcac874e47c135ea
ivanats <ivana.tsankova1@gmail.com>
2020-11-04 16:19:29 +0000

    ss

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,26 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+    System.out.println( " Enter your number:");
+    
+    int num = input.nextInt();
+    String number = num.toString();
+    String reversedNumber = " ";
+    assert num > 0 : " Number is not definiert ";
+
+    for(int i = number.lenght() -1; i >= 0; i--){ 
+      reversedNumber += number.charAt(i);
+    }
+      boolean result = true;
+    
+    for(int i = 0; i < number.lenght(); i++){
+      if (!number.charAt(i) == reversedNumber.charAt(i) ){
+       result = false;
+       break;
+      }
+    }
+       System.out.println ("Palindrome:" + result );
+   
+
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,38 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+       boolean result = true;
+       String password = input.nextLine();     
+       int digitCount = 0;
+
+    while(true){
+      if (password.length() < 8){ 
+        result =false;
+      } 
+      else{
+        for (int i = 0; i < password.length(); i++ ) {
+         if( Character.isLetterOrDigit(password.charAt(i))) {
+          digitCount++;
+         }
+         if(! Character.isLetterOrDigit(password.charAt(i))) {
+          result = false;
+          break; 
+         }
+        } 
+      } 
+
+  
+      if (digitCount < 2){
+        result = false;
+      }
+        
+      if(result){
+         System.out.println("Password is valid");
+        break;
+      }  else {
+          System.out.println("Password is invalid");
+          break;
+      }
     }
+}
 }
